0554ca99046e98d345c2aeecc939fc0ff445c044
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-07-20 12:05:25 -0600

    my edit

--- server/index.js
+++ server/index.js
@@ -9,6 +9,16 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
+  accessToken: '78be0bbabb1c494cb1040e52779c41d6',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+rollbar.log("Hello world!");
+
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"/public/index.html"));
   });
